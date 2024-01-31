2006895acd97dd76b47edb6fb9e72253f1489f51
Non Nontra <fantasyzernontra@hotmail.com>
2024-02-01 00:27:10 +0700

    update MWA Dashboard

--- src/Presentation/Views/Dashboard/MWA/View.tsx
+++ src/Presentation/Views/Dashboard/MWA/View.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { PageTitle } from '@/_metronic/layout/core'
+import useViewModel from './ViewModel'
 
 const MwaMeasurementDashboardView: React.FC = () => {
-	return <React.Fragment></React.Fragment>
+	const { timeStr } = useViewModel()
+
+	return (
+		<React.Fragment>
+			<PageTitle
+				description={timeStr}
+				aditionalDescription="Your current sales summary and activity.">
+				การประปานครหลวง
+			</PageTitle>
+		</React.Fragment>
+	)
 }
 
 export default MwaMeasurementDashboardView
--- src/Presentation/Views/Dashboard/Overview/View.tsx
+++ src/Presentation/Views/Dashboard/Overview/View.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { PageTitle } from '@/_metronic/layout/core'
 import useViewModel from './ViewModel'
 
 const DashboardOverviewView: React.FC = () => {
-	const { timeStr } = useViewModel()
+	useViewModel()
 
-	return (
-		<React.Fragment>
-			<PageTitle
-				description={timeStr}
-				aditionalDescription="Your current sales summary and activity.">
-				การประปานครหลวง
-			</PageTitle>
-		</React.Fragment>
-	)
+	return <React.Fragment></React.Fragment>
 }
 
 export default DashboardOverviewView
